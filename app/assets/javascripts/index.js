efcaba05e22172fe445c5f777c2ffd6d6e4c05f9
ryanden2018 <denlinger.ryan@gmail.com>
2019-06-03 10:35:38 -0500

    dumbbells

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -14,18 +14,39 @@ window.onload = function() {
     window.requestAnimationFrame(main);
     db.update();
     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for(var i=0; i<db.springs.length; i++) {
+      var x1 = db.springs[i].disk1.centerX;
+      var x2 = db.springs[i].disk2.centerX;
+      var y1 = db.springs[i].disk1.centerY;
+      var y2 = db.springs[i].disk2.centerY;
+      context.beginPath();
+      context.moveTo(x1,y1);
+      context.lineTo(x2,y2);
+      context.lineWidth = 2;
+      context.stroke();
+    }
+
+
     for(var i = 0; i < db.disks.length; i++) {
       context.beginPath();
       context.arc(Math.floor(db.disks[i].centerX),Math.floor(db.disks[i].centerY),
         Math.floor(db.disks[i].radius),
         0,2*Math.PI,false);
-      if(i % 10 === 0) {
+      if((i % 10 === 0) || ((i-1)%10 === 0)) {
+        context.fillStyle = "black";
         context.fill();
       } else {
+        context.strokeStyle = "black";
         context.lineWidth = 2;
         context.stroke();
+        context.fillStyle = "white";
+        context.fill();
+
       }
     }
+
+
   }
   
   main(0);
